7ba44f6ba80dd8f9c198637f57cfe273c80d0792
kyk <86296198+Msolmaz4@users.noreply.github.com>
2022-03-07 22:47:22 +0100

    search

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios'
 
 function App() {
+
+
+const API_URL =' http://www.omdbapi.com/?i=tt3896198&apikey=92cd55bd'
+
+
+const searchMovies = async (title)=>{
+const response = await fetch(`${API_URL}&s=${title}`);
+const data = await response.json();
+console.log(data.Search)
+        
+}
+
+
+
+useEffect(()=>{
+searchMovies('spiderman')
+},[])
+
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+
+
+
+
+      App
+     </div>
   );
 }
 
